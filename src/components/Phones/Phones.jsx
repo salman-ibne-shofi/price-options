bb18c86890cfd2ae259fa08b5c9ea1593afe878a
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-07-12 06:16:08 +0600

    explore react awesome components

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts";
+import { Grid } from "react-loader-spinner";
 
 const Phones = () => {
 	const [phones, setPhones] = useState([]);
+	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
 		// fetch("https://openapi.programming-hero.com/api/phones?search=iphone")
@@ -25,17 +27,30 @@ const Phones = () => {
 				});
 				console.log(phonesWithFkakeData);
 				setPhones(phonesWithFkakeData);
+				setLoading(false);
 			});
 	}, []);
 
 	return (
 		<div>
+			{loading && (
+				<Grid
+					visible={true}
+					height="80"
+					width="80"
+					color="#4fa94d"
+					ariaLabel="grid-loading"
+					radius="12.5"
+					wrapperStyle={{}}
+					wrapperClass="grid-wrapper"
+				/>
+			)}
 			<h2 className="text-5xl">Phones: {phones.length}</h2>
 			<BarChart width={1200} height={400} data={phones}>
 				<Bar dataKey="price" fill="#8884d8" />
-                <XAxis dataKey='name'></XAxis>
-                <YAxis></YAxis>
-                <Tooltip></Tooltip>
+				<XAxis dataKey="name"></XAxis>
+				<YAxis></YAxis>
+				<Tooltip></Tooltip>
 			</BarChart>
 		</div>
 	);
